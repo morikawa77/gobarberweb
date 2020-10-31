16731c3a9b95efe9678973557b7c97a2f46e07c6
Reginaldo Morikawa <morikawa77@gmail.com>
2020-10-30 23:36:25 -0300

    Rotas autenticadas

---
+++ src/routes/Route.tsx
@@ -0,0 +1,45 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable react/jsx-indent */
+import React from 'react';
+import {
+  RouteProps as ReactDOMRouteProps,
+  Route as ReactDOMRoute,
+  Redirect,
+} from 'react-router-dom';
+
+import { useAuth } from '../hooks/auth';
+
+interface RouteProps extends ReactDOMRouteProps {
+  isPrivate?: boolean;
+  component: React.ComponentType;
+}
+
+const Route: React.FC<RouteProps> = ({
+  isPrivate = false,
+  component: Component,
+  ...rest
+}) => {
+  const { user } = useAuth();
+
+  console.log(user, isPrivate);
+
+  return (
+    <ReactDOMRoute
+      {...rest}
+      render={({ location }) => {
+        return isPrivate === !!user ? (
+          <Component />
+        ) : (
+            <Redirect
+              to={{
+                pathname: isPrivate ? '/' : '/dashboard',
+                state: { from: location },
+              }}
+            />
+          );
+      }}
+    />
+  );
+};
+
+export default Route;
