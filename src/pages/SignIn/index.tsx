c6759332e9abfc2e1651520cc96182aac48a380e
Reginaldo Morikawa <morikawa77@gmail.com>
2020-10-30 21:52:17 -0300

    Animando Toasts

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -53,7 +53,11 @@ const SignIn: React.FC = () => {
           formRef.current?.setErrors(errors);
         }
 
-        addToast();
+        addToast({
+          type: 'error',
+          title: 'Erro na autenticação',
+          description: 'Ocorreu um erro a fazer login, cheque as credenciais.',
+        });
       }
     },
     [signIn, addToast],
